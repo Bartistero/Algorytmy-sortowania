edc61937f2638793ea1936c796861019ae2a2267
Bartistero <45877854+Bartistero@users.noreply.github.com>
2019-03-10 17:24:53 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> /* plik nagłówkowy */
+using namespace std;
+
+int main() //główny program
+{
+    cout << "Hello world!"; //wypisanie na standardowe wyjście
+    return 0; //koniec programu - zwracam 0
+}
